93228d4a510028a6e3910b949f8d6cd69a9a785a
JasonTsai <jason.tsaicc@gmail.com>
2025-01-20 16:46:12 +0800

    docs: add architecture diagram for testing environment

--- modules/eks/main.tf
+++ modules/eks/main.tf
@@ -28,26 +28,24 @@ resource "aws_iam_role_policy_attachment" "eks_cluster_AmazonEKSClusterPolicy" {
 #   name        = "${var.project_name}-eks-cluster-sg"
 #   vpc_id      = var.vpc_id
 #   description = "Security group for EKS Cluster"
-
-#   ingress {
-#     from_port   = 443
-#     to_port     = 443
-#     protocol    = "tcp"
-#     cidr_blocks = ["0.0.0.0/0"]
+#   dynamic "ingress" {
+#     for_each = var.ingress_rules
+#     content {
+#       from_port   = ingress.value.from_port
+#       to_port     = ingress.value.to_port
+#       protocol    = ingress.value.protocol
+#       cidr_blocks = ingress.value.cidr_blocks
+#     }
 #   }
 
-#   ingress {
-#     from_port   = 1025
-#     to_port     = 65535
-#     protocol    = "tcp"
-#     cidr_blocks = ["0.0.0.0/0"]
-#   }
-
-#   egress {
-#     from_port   = 0
-#     to_port     = 0
-#     protocol    = "-1"
-#     cidr_blocks = ["0.0.0.0/0"]
+#   dynamic "egress" {
+#     for_each = var.egress_rules
+#     content {
+#       from_port   = egress.value.from_port
+#       to_port     = egress.value.to_port
+#       protocol    = egress.value.protocol
+#       cidr_blocks = egress.value.cidr_blocks
+#     }
 #   }
 # }
 
--- modules/eks/variables.tf
+++ modules/eks/variables.tf
@@ -42,3 +42,22 @@ variable "node_scaling_config" {
   }
 }
 
+# variable "ingress_rules" {
+#   type = list(object({
+#     from_port   = number
+#     to_port     = number
+#     protocol    = string
+#     cidr_blocks = list(string)
+#   }))
+
+# }
+
+# variable "egress_rules" {
+#   type = list(object({
+#     from_port   = number
+#     to_port     = number
+#     protocol    = string
+#     cidr_blocks = list(string)
+#   }))
+
+# }
